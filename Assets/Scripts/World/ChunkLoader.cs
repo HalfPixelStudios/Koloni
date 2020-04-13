da83f117c63a80af395703bd0e0cad803f01f8c4
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-13 19:14:21 -0400

    render distance

--- Assets/Scripts/World/ChunkLoader.cs
+++ Assets/Scripts/World/ChunkLoader.cs
@@ -16,22 +16,22 @@ public class ChunkLoader : MonoBehaviour {
     Dictionary<Vector2, TerrainChunk> loaded_chunks = new Dictionary<Vector2, TerrainChunk>();
 
     void Start() {
-        visible_chunks = Mathf.RoundToInt(view_distance / visible_chunks);
+
         chunkSize = GetComponent<CubeMarch>().chunkSize; //TEMP SOLUTION
+        visible_chunks = Mathf.RoundToInt(view_distance / chunkSize);
     }
 
-    private void Update() {
-        Vector2 viewer_pos = new Vector2(viewer.transform.position.x,viewer.transform.position.z);
+    void Update() {
+        viewer_pos = new Vector2(viewer.transform.position.x,viewer.transform.position.z);
         UpdateVisibleChunks();
     }
 
     void UpdateVisibleChunks() {
         Vector2 viewerChunkCoord = new Vector2(Mathf.RoundToInt(viewer_pos.x/chunkSize), Mathf.RoundToInt(viewer_pos.y / chunkSize));
-        Debug.Log(viewer_pos);
-        for (int z = -visible_chunks; z <= visible_chunks; z++) {
-            for (int x = -visible_chunks; x <= visible_chunks; x++) {
-                Vector3 newChunkPos = new Vector2((int)(viewerChunkCoord.x+x),(int)(viewerChunkCoord.y+z));
-
+        for (int j = -visible_chunks; j <= visible_chunks; j++) {
+            for (int i = -visible_chunks; i <= visible_chunks; i++) {
+                Vector2 newChunkPos = new Vector2((int)(viewerChunkCoord.x+i),(int)(viewerChunkCoord.y+j));
+                Debug.Log(newChunkPos);
                 if (loaded_chunks.ContainsKey(newChunkPos)) { //if the chunk has already been generated
 
                     TerrainChunk chunk = loaded_chunks[newChunkPos];
@@ -49,18 +49,18 @@ public class ChunkLoader : MonoBehaviour {
 
     public class TerrainChunk {
 
-        Vector3 position;
+        //Vector2 position;
         GameObject meshObject;
 
         Bounds bounds; //used for finding distance
 
-        public TerrainChunk(Vector3 position, int chunkSize) {
-            this.position = position;
-            bounds = new Bounds(this.position, Vector2.one * chunkSize);
-
+        public TerrainChunk(Vector2 grid_pos, int chunkSize) {
+            Vector2 pos = grid_pos * chunkSize;
+            //this.position = position;
+            bounds = new Bounds(pos, Vector2.one * chunkSize);
 
             meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
-            meshObject.transform.position = this.position;
+            meshObject.transform.position = new Vector3(pos.x,0,pos.y);
             meshObject.transform.localScale = Vector3.one * chunkSize / 10f;
             this.setVisible(false);
         }
