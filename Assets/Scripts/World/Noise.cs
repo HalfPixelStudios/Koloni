3b11da392f4938bb67c868697ec809bb1199c4f6
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-13 21:04:00 -0400

    ALL SEAMS FIXED YAYYY

--- Assets/Scripts/World/Noise.cs
+++ Assets/Scripts/World/Noise.cs
@@ -7,21 +7,24 @@ public class Noise  {
     public static float[,,] GenerateNoiseMap(int seed, int chunkSize, int chunkHeight, Vector3 noiseOffset, float noiseScale, float noiseWeight, int octaves, float persistance, float lacunarity) {
         System.Random prng = new System.Random(seed);
 
-        float maxPossibleHeight = 0; //estimate the range of values
+        float maxPossibleNoise = 0; //estimate the range of values
         float amp = 1;
         float freq = 1;
 
         Vector3[] octaveOffsets = new Vector3[octaves];
         
         for (int i = 0; i < octaves; i++) { //also offset each level of noise
-            octaveOffsets[i] = new Vector3(prng.Next(-100000,100000)+noiseOffset.x,prng.Next(-100000,100000)+noiseOffset.y,prng.Next(-100000,100000)-noiseOffset.z);
+            octaveOffsets[i] = new Vector3(prng.Next(-100000,100000)+noiseOffset.x,prng.Next(-100000,100000)+noiseOffset.y,prng.Next(-100000,100000)+noiseOffset.z);
 
-            maxPossibleHeight += amp;
+            maxPossibleNoise += amp;
             amp *= persistance;
 
         }
 
+        float maxPossibleHeight = chunkHeight + maxPossibleNoise * noiseWeight;
+        
         float[,,] noiseMap = new float[chunkSize + 1, chunkHeight + 1, chunkSize + 1];
+
 
         for (int z = 0; z < chunkSize + 1; z++) {
             for (int y = 0; y < chunkHeight + 1; y++) {
@@ -43,27 +46,25 @@ public class Noise  {
                         freq *= lacunarity;
                     }
 
-                    float terrainNoise = y + noise * noiseWeight;
-                    //clamp all noise values so they are between 0 and 1
-                    float finalNoise = (terrainNoise + 1)/(2f*maxPossibleHeight/1.75f);
+                    float finalNoise = y + noise * noiseWeight;
 
                     noiseMap[z, y, x] = finalNoise;
-
 
                 }
             }
         }
 
-        /*
+        
         //clamp all noise values so they are between 0 and 1
         for (int z = 0; z < chunkSize + 1; z++) {
             for (int y = 0; y < chunkHeight + 1; y++) {
                 for (int x = 0; x < chunkSize + 1; x++) {
-                    noiseMap[z, y, x] = Mathf.InverseLerp(minNoise,maxNoise,noiseMap[z,y,x]);
+                    //noiseMap[z, y, x] = Mathf.InverseLerp(minNoise,maxNoise,noiseMap[z,y,x]);
+                    noiseMap[z, y, x] = Mathf.InverseLerp(0, maxPossibleHeight, noiseMap[z, y, x]);
                 }
             }
         }
-        */
+        
         
         return noiseMap;
 
