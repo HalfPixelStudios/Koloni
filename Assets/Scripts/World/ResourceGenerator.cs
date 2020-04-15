914e71b3886966cdbf85127a1c70178d01c6cd2c
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-14 23:27:20 -0400

    made some rocks xdd

---
+++ Assets/Scripts/World/ResourceGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGenerator : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
